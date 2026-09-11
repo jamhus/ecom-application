a05425aa12cc250e146e8aba088f6cc6aaceb5bb
Jamal Hussein <jamal.1994@live.se>
2026-09-11 13:37:32 +0200

    add product entity and controller

--- src/main/java/com/app/ecomapplication/controller/UserController.java
+++ src/main/java/com/app/ecomapplication/controller/UserController.java
@@ -1,7 +1,8 @@
 package com.app.ecomapplication.controller;
 
+import com.app.ecomapplication.entity.dto.UserRequest;
+import com.app.ecomapplication.entity.dto.UserResponse;
 import com.app.ecomapplication.service.UserService;
-import com.app.ecomapplication.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,24 +16,26 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> GetUserByID(@PathVariable Long id) {
+    public ResponseEntity<UserResponse> GetUserByID(@PathVariable Long id) {
         return userService
                 .findUserById(id)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
     @GetMapping
-    public List<User> GetAllUsers() {
+    public List<UserResponse> GetAllUsers() {
         return userService.fetchAllUsers();
     }
 
     @PostMapping
-    public void CreateUser(@RequestBody User user) {
+    public void CreateUser(@RequestBody UserRequest user) {
+
         userService.createUser(user);
     }
 
     @PutMapping("/{id}")
-    public boolean updateUser(@PathVariable Long id, @RequestBody User model) {
+    public boolean updateUser(@PathVariable Long id, @RequestBody UserRequest model) {
         return userService.updateUser(id, model);
     }
+
 }
